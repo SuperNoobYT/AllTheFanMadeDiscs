537b247f6e194429a7689b6c16ae08c92f534b03
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2022-03-18 16:58:49 +0100

    redo bedrock structure feature
    - no more laggy structures generation yey

--- src/main/java/com/supernoob/atfmd/object/StructuresFeatures/BedrockDiscStructure.java
+++ src/main/java/com/supernoob/atfmd/object/StructuresFeatures/BedrockDiscStructure.java
@@ -12,44 +12,31 @@ import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 import org.apache.logging.log4j.Level;
 
 import java.util.Optional;
-import java.util.Random;
 
 public class BedrockDiscStructure extends StructureFeature<StructurePoolFeatureConfig> {
 
     public BedrockDiscStructure() {
         super(StructurePoolFeatureConfig.CODEC, BedrockDiscStructure::createPiecesGenerator, PostPlacementProcessor.EMPTY);
     }
-    public static int randomHeight() {
-        Random random = new Random();
-        int minY = ATFMD.CONFIG.stuff.Min_Y;
-        int maxY = ATFMD.CONFIG.stuff.Max_Y;
-        return random.nextInt(maxY - minY) + minY;
-    }
-    private static boolean isFeatureChunk(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
-        int y = BedrockDiscStructure.randomHeight();
+
+    public static Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> createPiecesGenerator(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
+        int y = ATFMD.CONFIG.stuff.Min_Y;
         BlockPos spawnXZPosition = context.chunkPos().getCenterAtY(y);
         int x = spawnXZPosition.getX();
         int z = spawnXZPosition.getZ();
-        int a = 0;
-        for (int x1 = x - 9; x1 <= x + 9; x1++) {
-            for (int y1 = y; y1 <= y + 4; y1++) {
-                for (int z1 = z - 9; z1 <= z + 9; z1++) {
-                    VerticalBlockSample columnOfBlocks = context.chunkGenerator().getColumnSample(x1, z1, context.world());
-                    BlockState areablock = columnOfBlocks.getState(y1);
-                    if (areablock.getBlock()== Blocks.AIR){
-                        a++;
-                    }
-                }
+        VerticalBlockSample columnOfBlocks = context.chunkGenerator().getColumnSample(x, z, context.world());
+        while (y<=ATFMD.CONFIG.stuff.Max_Y) {
+            y++;
+            BlockState areablock = columnOfBlocks.getState(y);
+            if (areablock.getBlock() == Blocks.AIR) {
+                break;
             }
         }
-        return a>3;
-    }
-    public static Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> createPiecesGenerator(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
-        if (!BedrockDiscStructure.isFeatureChunk(context)) {
+        if (y>ATFMD.CONFIG.stuff.Max_Y) {
             return Optional.empty();
         }
         BlockPos blockpos = context.chunkPos().getCenterAtY(0);
-        blockpos = blockpos.up(randomHeight());
+        blockpos = blockpos.up(y);
 
         Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> structurePiecesGenerator =
                 StructurePoolBasedGenerator.generate(
