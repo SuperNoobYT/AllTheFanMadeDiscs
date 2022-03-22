4b803a4f37ec592a1a3e0f06893aaab032f3deea
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2022-03-22 20:40:35 +0100

    Fix Structures issue
    - updated loottable
    - fixed missing structure void
    - fixed laggy generation
    - delete extra useless unoptimize check

--- src/main/java/com/supernoob/atfmd/object/StructuresFeatures/BedrockDiscStructure.java
+++ src/main/java/com/supernoob/atfmd/object/StructuresFeatures/BedrockDiscStructure.java
@@ -2,8 +2,6 @@ package com.supernoob.atfmd.object.StructuresFeatures;
 
 import com.mojang.serialization.Codec;
 import com.supernoob.atfmd.ATFMD;
-import com.supernoob.atfmd.config.ModConfig;
-import net.minecraft.block.BlockState;
 import net.minecraft.structure.MarginedStructureStart;
 import net.minecraft.structure.PoolStructurePiece;
 import net.minecraft.structure.StructureManager;
@@ -16,12 +14,8 @@ import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.HeightLimitView;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.source.BiomeSource;
-import net.minecraft.world.gen.ChunkRandom;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.chunk.VerticalBlockSample;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
@@ -42,14 +36,6 @@ public class BedrockDiscStructure extends StructureFeature<DefaultFeatureConfig>
         int maxY = ATFMD.CONFIG.stuff.Max_Y;
         return random.nextInt(maxY - minY) + minY;
     }
-    @Override
-    protected boolean shouldStartAt(ChunkGenerator chunkGenerator, BiomeSource biomeSource, long seed, ChunkRandom chunkRandom, ChunkPos chunkPos, Biome biome, ChunkPos chunkPos2, DefaultFeatureConfig featureConfig, HeightLimitView heightLimitView) {
-        BlockPos centerOfChunk = new BlockPos(chunkPos.x * 16, randomHeight(), chunkPos.z * 16);
-        int landHeight = chunkGenerator.getHeightInGround(centerOfChunk.getX(), centerOfChunk.getZ(), Heightmap.Type.WORLD_SURFACE_WG, heightLimitView);
-        VerticalBlockSample columnOfBlocks = chunkGenerator.getColumnSample(centerOfChunk.getX(), centerOfChunk.getZ(), heightLimitView);
-        BlockState topBlock = columnOfBlocks.getState(centerOfChunk.up(landHeight));
-        return topBlock.getFluidState().isEmpty();
-    }
 
     public static class Start extends MarginedStructureStart<DefaultFeatureConfig> {
         public Start(StructureFeature<DefaultFeatureConfig> structureIn, ChunkPos chunkPos, int referenceIn, long seedIn) {
@@ -61,21 +47,6 @@ public class BedrockDiscStructure extends StructureFeature<DefaultFeatureConfig>
             int z = chunkPos.z * 16;
             int y = BedrockDiscStructure.randomHeight();
                 BlockPos centerPos = new BlockPos.Mutable(x, y, z);
-                int a = 0;
-                for(int x1 = x-5; x1<=x+5; x1++){
-                    for(int y1 = y; y1<=y+4; y1++){
-                        for(int z1 = z-5; z1<=z+5; z1++){
-                            BlockPos centerchunk = new BlockPos(x1,y1,z1);
-                            VerticalBlockSample columnofblock1 = chunkGenerator.getColumnSample(x1, z1, heightLimitView);
-                            BlockState areablock = columnofblock1.getState(centerchunk.up(y1));
-                            if(!areablock.getFluidState().isEmpty()){
-                                a++;
-                            }
-                        }
-                    }
-                }
-
-                if(a==0) {
                     StructurePoolFeatureConfig structureSettingsAndStartPool = new StructurePoolFeatureConfig(() -> dynamicRegistryManager.get(Registry.STRUCTURE_POOL_KEY)
                             .get(new Identifier(ATFMD.MOD_ID, "bedrock_disc/start_pool")),
                             1);
@@ -93,7 +64,7 @@ public class BedrockDiscStructure extends StructureFeature<DefaultFeatureConfig>
                             false,
                             heightLimitView);
 
-                    this.children.forEach(piece -> piece.translate(0, 1, 0));
+                    this.children.forEach(piece -> piece.translate(0, 0, 0));
 
                     Vec3i structureCenter = this.children.get(0).getBoundingBox().getCenter();
                     int xOffset = centerPos.getX() - structureCenter.getX();
@@ -111,4 +82,3 @@ public class BedrockDiscStructure extends StructureFeature<DefaultFeatureConfig>
                 }
         }
     }
-}
