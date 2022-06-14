0be142f90e2cf3dcd6bae538aebb810ff88ba0d4
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2022-06-14 10:56:24 +0200

    Updated loottable to 1.19

--- src/main/java/com/supernoob/atfmd/object/LootTable.java
+++ src/main/java/com/supernoob/atfmd/object/LootTable.java
@@ -1,7 +1,8 @@
 package com.supernoob.atfmd.object;
 
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.util.Identifier;
@@ -9,12 +10,12 @@ import net.minecraft.util.Identifier;
 public class LootTable {
 
     public static void ModifyLootTable(Identifier baseloottable, Identifier addedloottable) {
-        LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+        LootTableEvents.MODIFY.register((resourceManager, manager, id, supplier, setter) -> {
             if (baseloottable.equals(id)) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.copyOf(LootPool.builder().build())
                         .rolls(ConstantLootNumberProvider.create(1))
                         .with(LootTableEntry.builder(addedloottable));
-                supplier.withPool(poolBuilder.build());
+                supplier.build();
             }
         });
     }
