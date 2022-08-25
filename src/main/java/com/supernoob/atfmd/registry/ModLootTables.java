5d8a9c51accbb3a8e3f65575bbd6fce83aa6e717
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2022-08-25 19:25:55 +0200

    New loot Systeme
    use loottable to make mobs drop discs in specific condition

--- src/main/java/com/supernoob/atfmd/registry/ModLootTables.java
+++ src/main/java/com/supernoob/atfmd/registry/ModLootTables.java
@@ -20,6 +20,8 @@ public class ModLootTables {
     public static final Identifier IGLOO_CHEST_ID = new Identifier("minecraft", "chests/igloo_chest");
     public static final Identifier JUNGLE_TEMPLE_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "chests/jungle_temple");
     public static final Identifier JUNGLE_TEMPLE_CHEST_ID = new Identifier("minecraft", "chests/jungle_temple");
+    public static final Identifier PHANTOM_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/phantom");
+    public static final Identifier PHANTOM_ID = new Identifier("minecraft", "entities/phantom");
     public static final Identifier PILLAGER_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "chests/pillager_outpost");
     public static final Identifier PILLAGER_OUTPOST_ID = new Identifier("minecraft", "chests/pillager_outpost");
     public static final Identifier SHIPWRECK_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "chests/shipwreck_treasure");
@@ -32,6 +34,10 @@ public class ModLootTables {
     public static final Identifier ENDER_DRAGON_ID = new Identifier("minecraft", "entities/ender_dragon");
     public static final Identifier FINALE_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/wither");
     public static final Identifier WITHER_ID = new Identifier("minecraft", "entities/wither");
+    public static final Identifier FOURTEEN_REVAMP_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/zombie");
+    public static final Identifier ZOMBIE_ID = new Identifier("minecraft", "entities/zombie");
+    public static final Identifier PRISMARINE_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/elder_guardian");
+    public static final Identifier ELDER_GUARDIAN_ID = new Identifier("minecraft", "entities/elder_guardian");
 
     public static void init() {
         LootTable.ModifyLootTable(ABANDONED_MINESHAFT_ID, STRING_LOOTTABLE_ID);
@@ -41,11 +47,14 @@ public class ModLootTables {
         LootTable.ModifyLootTable(IGLOO_CHEST_ID, IGLOO_LOOTTABLE_ID);
         LootTable.ModifyLootTable(JUNGLE_TEMPLE_CHEST_ID, JUNGLE_TEMPLE_LOOTTABLE_ID);
         LootTable.ModifyLootTable(NETHER_BRIDGE_ID, FORT_STRESS_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(PHANTOM_ID, PHANTOM_LOOTTABLE_ID);
         LootTable.ModifyLootTable(PILLAGER_OUTPOST_ID, PILLAGER_LOOTTABLE_ID);
         LootTable.ModifyLootTable(SHIPWRECK_TRASURE_ID, SHIPWRECK_LOOTTABLE_ID);
         LootTable.ModifyLootTable(UNDERWATER_RUIN_BIG_ID, UNDERWATER_RUIN_LOOTTABLE_ID);
         LootTable.ModifyLootTable(WOODLAND_MANSION_ID, MANSION_LOOTTABLE_ID);
         LootTable.ModifyLootTable(ENDER_DRAGON_ID, ENDERWAVE_LOOTTABLE_ID);
         LootTable.ModifyLootTable(WITHER_ID, FINALE_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(ZOMBIE_ID, FOURTEEN_REVAMP_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(ELDER_GUARDIAN_ID, PRISMARINE_LOOTTABLE_ID);
     }
 }
