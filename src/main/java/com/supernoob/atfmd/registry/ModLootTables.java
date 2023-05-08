70066d4a501391105948e6b7d03eceb08e07e076
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2023-05-08 09:29:53 +0200

    clean up 1.19.2

--- src/main/java/com/supernoob/atfmd/registry/ModLootTables.java
+++ src/main/java/com/supernoob/atfmd/registry/ModLootTables.java
@@ -44,6 +44,10 @@ public class ModLootTables {
     public static final Identifier WITCH_ID = new Identifier("minecraft", "entities/witch");
     public static final Identifier VIBRATE_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/warden");
     public static final Identifier WARDEN_ID = new Identifier("minecraft", "entities/warden");
+    public static final Identifier THE_LOST_SOUL_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "entities/ghast");
+    public static final Identifier GHAST_ID = new Identifier("minecraft", "entities/ghast");
+    public static final Identifier CARROT_LOOTTABLE_ID = new Identifier(ATFMD.MOD_ID, "blocks/carrots");
+    public static final Identifier CARROTS_ID = new Identifier("minecraft", "blocks/carrots");
 
     public static void init() {
         LootTable.ModifyLootTable(ABANDONED_MINESHAFT_ID, STRING_LOOTTABLE_ID);
@@ -65,5 +69,8 @@ public class ModLootTables {
         LootTable.ModifyLootTable(ELDER_GUARDIAN_ID, PRISMARINE_LOOTTABLE_ID);
         LootTable.ModifyLootTable(WITCH_ID, TRICKS_LOOTTABLE_ID);
         LootTable.ModifyLootTable(WARDEN_ID, VIBRATE_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(WARDEN_ID, VIBRATE_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(GHAST_ID, THE_LOST_SOUL_LOOTTABLE_ID);
+        LootTable.ModifyLootTable(CARROTS_ID, CARROT_LOOTTABLE_ID);
     }
 }
