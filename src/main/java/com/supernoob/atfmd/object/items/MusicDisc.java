52effbdb271eb0994d126614442d8b92cbe24705
SuperNoobYT <79205494+SuperNoobYT@users.noreply.github.com>
2023-05-08 10:45:43 +0200

    add fr_fr

--- src/main/java/com/supernoob/atfmd/object/items/MusicDisc.java
+++ src/main/java/com/supernoob/atfmd/object/items/MusicDisc.java
@@ -4,8 +4,8 @@ import net.minecraft.item.MusicDiscItem;
 import net.minecraft.sound.SoundEvent;
 
 
-public class MusicDisc extends MusicDiscItem {
 
+public class MusicDisc extends MusicDiscItem {
     public MusicDisc(int comparatorOutput, SoundEvent sound, Settings settings, int lengthInSeconds) {
         super(comparatorOutput, sound, settings, lengthInSeconds);
     }
